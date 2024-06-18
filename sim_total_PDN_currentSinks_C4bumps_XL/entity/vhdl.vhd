f40f1d9f44cce1784e2e403b9f4e4c2f56c3c972
mikefun1 <124843697+mikefun1@users.noreply.github.com>
2024-06-17 20:09:40 -0400

    Add files via upload
    last of the final project files

---
+++ sim_total_PDN_currentSinks_C4bumps_XL/entity/vhdl.vhd
@@ -0,0 +1,10 @@
+
+--Create Entity:
+--Library=ECE473,Cell=sim_total_PDN_currentSinks_C4bumps_XL,View=entity
+--Time:Mon Jun 10 20:34:29 2024
+--By:mlw375
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+ENTITY \sim_total_PDN_currentSinks_C4bumps_XL\ IS
+END \sim_total_PDN_currentSinks_C4bumps_XL\;
---
+++ sim_total_PDN_currentSinks_C4bumps_XL/systemVerilog/verilog.sv
@@ -0,0 +1,6 @@
+//systemVerilog HDL for "ECE473", "sim_total_PDN_currentSinks_C4bumps_XL" "systemVerilog"
+
+
+module sim_total_PDN_currentSinks_C4bumps_XL ( );
+
+endmodule
